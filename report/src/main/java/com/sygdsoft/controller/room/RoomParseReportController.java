03803488c1307e769d941880b1e05ce01b4b9c25
shuzhan <sichengde1@163.com>
2017-02-12 19:37:12 +0800

    客房销售统计增加合计

--- report/src/main/java/com/sygdsoft/controller/room/RoomParseReportController.java
+++ report/src/main/java/com/sygdsoft/controller/room/RoomParseReportController.java
@@ -41,6 +41,9 @@ public class RoomParseReportController {
     GroupIntegrationService groupIntegrationService;
     @Autowired
     GuestSourceService guestSourceService;
+    private Integer rentSubmit;
+    private Integer totalSubmit;
+    private Double consumeSubmit;
 
     @RequestMapping("RoomParseReport")
     public List<RoomParseReportRow> RoomParseReport(@RequestBody RoomParseReportQuery roomParseReportQuery) throws Exception {
@@ -53,6 +56,7 @@ public class RoomParseReportController {
                 /*分为3个10天*/
                 beginTime = timeService.getMinMonth(date);
                 endTime = timeService.addDay(beginTime, 10);
+                this.clearSubmit();
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "上旬");
                 beginTime = endTime;
                 endTime = timeService.addDay(beginTime, 10);
@@ -60,11 +64,13 @@ public class RoomParseReportController {
                 beginTime = endTime;
                 endTime = timeService.getMaxMonth(endTime);
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "下旬");
+                this.parseSubmit(roomParseReportRowList);
                 break;
             case "季":
                 /*分为3个月，每个月3个10天*/
                 beginTime = timeService.getMinQuarter(date);
                 endTime = timeService.addDay(beginTime, 10);
+                this.clearSubmit();
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "上旬");
                 beginTime = endTime;
                 endTime = timeService.addDay(beginTime, 10);
@@ -72,9 +78,10 @@ public class RoomParseReportController {
                 beginTime = endTime;
                 endTime = timeService.getMaxMonth(endTime);
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "下旬");
-
+                this.parseSubmit(roomParseReportRowList);
                 beginTime = endTime;
                 endTime = timeService.addDay(beginTime, 10);
+                this.clearSubmit();
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "上旬");
                 beginTime = endTime;
                 endTime = timeService.addDay(beginTime, 10);
@@ -82,9 +89,10 @@ public class RoomParseReportController {
                 beginTime = endTime;
                 endTime = timeService.getMaxMonth(endTime);
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "下旬");
-
+                this.parseSubmit(roomParseReportRowList);
                 beginTime = endTime;
                 endTime = timeService.addDay(beginTime, 10);
+                this.clearSubmit();
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "上旬");
                 beginTime = endTime;
                 endTime = timeService.addDay(beginTime, 10);
@@ -92,17 +100,22 @@ public class RoomParseReportController {
                 beginTime = endTime;
                 endTime = timeService.getMaxMonth(endTime);
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime) + "下旬");
-
+                this.parseSubmit(roomParseReportRowList);
                 break;
             case "年":
                 /*分为4个季度，每个季度3个月*/
                 beginTime = timeService.getMinYear(date);
                 endTime = timeService.addMonth(beginTime, 1);
+                this.clearSubmit();
                 this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime));
                 for (int i = 0; i < 11; i++) {
                     beginTime = endTime;
                     endTime = timeService.addMonth(beginTime, 1);
                     this.parseData(beginTime, endTime, roomParseReportRowList, timeService.getMonthCN(beginTime));
+                    if(i%3==1){
+                        this.parseSubmit(roomParseReportRowList);
+                        this.clearSubmit();
+                    }
                 }
                 break;
         }
@@ -118,6 +131,9 @@ public class RoomParseReportController {
             Double allDayConsume = szMath.nullToZero(debtIntegrationService.getSumAllDayConsumeByDate(beginTime, endTime));
             roomParseReportRow.setAveragePrice(szMath.formatTwoDecimal(allDayConsume, roomStateReport.getRent()));//设置平均房价
             roomParseReportRow.setRevper(szMath.formatTwoDecimal(allDayConsume, roomStateReport.getTotal()));//设置REVPER
+            this.rentSubmit += roomStateReport.getRent();
+            this.totalSubmit += roomStateReport.getTotal();
+            this.consumeSubmit += allDayConsume;
         }
         roomParseReportRow.setGuestNum(szMath.nullToZero(checkInIntegrationService.getSumNumByDate(beginTime, endTime)));//接待总人数
         roomParseReportRow.setGroupNum(szMath.nullToZero(groupIntegrationService.getSumByDate(beginTime, endTime)));//团队接待数量
@@ -126,7 +142,7 @@ public class RoomParseReportController {
         List<String> incomeTitleList = new ArrayList<>();
         /*分析客源房费*/
         List<DebtIntegration> debtIntegrationList = debtIntegrationService.getSumRoomConsumeByDateGuestSource(beginTime, endTime);
-        Integer index=0;
+        Integer index = 0;
         for (DebtIntegration debtIntegration : debtIntegrationList) {
             if (debtIntegration.getGuestSource() == null) {
                 continue;
@@ -154,10 +170,25 @@ public class RoomParseReportController {
             incomeList.add(ifNotNullGetString(debtIntegration.getConsume()));
             incomeTitleList.add(debtIntegration.getPointOfSale());
             incomeList.add(debtIntegration.getCount());
-            incomeTitleList.add(debtIntegration.getPointOfSale()+"次数");
+            incomeTitleList.add(debtIntegration.getPointOfSale() + "次数");
         }
         roomParseReportRow.setIncome(incomeList);
         roomParseReportRow.setIncomeTitle(incomeTitleList);
+        roomParseReportRowList.add(roomParseReportRow);
+    }
+
+    private void clearSubmit() {
+        this.rentSubmit = 0;
+        this.totalSubmit = 0;
+        this.consumeSubmit = 0.0;
+    }
+
+    private void parseSubmit(List<RoomParseReportRow> roomParseReportRowList) {
+        RoomParseReportRow roomParseReportRow = new RoomParseReportRow();
+        roomParseReportRow.setMonth("小计");//设置左边第一栏
+        roomParseReportRow.setAverageRent(szMath.formatTwoDecimal(this.rentSubmit, this.totalSubmit));//设置住客率
+        roomParseReportRow.setAveragePrice(szMath.formatTwoDecimal(this.consumeSubmit, this.rentSubmit));//设置平均房价
+        roomParseReportRow.setRevper(szMath.formatTwoDecimal(this.consumeSubmit, this.rentSubmit));//设置REVPER
         roomParseReportRowList.add(roomParseReportRow);
     }
 }
