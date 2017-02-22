d6178cd776a8b501d27a9fc3eaa52ede74624e5d
shuzhan <sichengde1@163.com>
2017-02-22 21:40:48 +0800

    单位按明细结算

--- release/src/main/resources/static/app/reception/companyDebt/companyPay/companyPayController.js
+++ release/src/main/resources/static/app/reception/companyDebt/companyPay/companyPayController.js
@@ -6,9 +6,24 @@ App.controller('companyPayController', ['$scope', 'webService', 'dataService', '
     $scope.company = company.name;
     $scope.currencyPayList = [];//sz-pay
     $scope.currencyPayList[0] = {};//使用sz-pay必须在父控制器声明这两个变量
-    dataService.initData(['refreshCompanyList', 'refreshCurrencyList'], [{condition: 'if_debt=\'y\''}, {condition: 'check_in=1'}])
+    $scope.initConditionCompany='company=' + util.wrapWithBrackets(company.name);
+    //账务明细
+    $scope.companyDebtFields = [
+        {name: '单位名称', id: 'company', width: '230px'},
+        {name: '签单人', id: 'lord', width: '100px'},
+        {name: '结账时间', id: 'doTime', date: 'true', width: '100px', desc: '0', filter: 'date'},
+        {name: '挂账款', id: 'debt', width: '100px'},
+        {name: '剩余挂账', id: 'currentRemain', width: '100px'},
+        {name: '币种', id: 'currency', width: '150px', filter: 'list'},
+        {name: '币种信息', id: 'currencyAdd', width: '150px', filter: 'list'},
+        {name: '操作类别', id: 'category', width: '150px', filter: 'list'},
+        {name: '结账流水号', id: 'paySerial', width: '150px'},
+        {name: '备注', id: 'description', width: '150px'},
+        {name: '营业部门', id: 'pointOfSale', width: '150px'},
+        {name: '操作员', id: 'userId', width: '120px'}
+    ];
+    dataService.initData([ 'refreshCurrencyList'], [ {condition: 'check_in=1'}])
         .then(function () {
-            $scope.companyList = util.objectListToString(dataService.getCompanyList(), 'name');
             $scope.currencyList = util.objectListToString(dataService.getCurrencyList(), 'currency');
             $scope.currencyList.push('转单位');
             $scope.currency = '人民币';
