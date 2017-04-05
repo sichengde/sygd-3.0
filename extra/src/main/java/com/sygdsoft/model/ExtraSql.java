dd6b7d052b8e6908b8a2eef2805000f7aa0c1ec0
shuzhan <2651283633@qq.com>
2017-04-05 10:56:41 +0800

    梧桐提成表房吧品种聚合

--- extra/src/main/java/com/sygdsoft/model/ExtraSql.java
+++ extra/src/main/java/com/sygdsoft/model/ExtraSql.java
@@ -10,7 +10,7 @@ public class ExtraSql {
     }
     public String getList(Map<String,Object> params){
         String userId=(String) params.get("userId");
-        String basic="SELECT  item,num,total_money totalMoney,unit,ifnull(currency,\'挂账\') currency FROM room_shop_detail LEFT JOIN debt_pay ON room_shop_detail.pay_serial = debt_pay.pay_serial where do_time>#{beginTime} and do_time < #{endTime}";
+        String basic="SELECT  item,sum(num) num,sum(total_money) totalMoney,unit,ifnull(currency,'挂账') currency FROM room_shop_detail LEFT JOIN debt_pay ON room_shop_detail.pay_serial = debt_pay.pay_serial where do_time>#{beginTime} and do_time < #{endTime} GROUP BY item,unit,currency";
         if(userId!=null){
             basic+=" and room_shop_detail.user_id=#{userId}";
         }
