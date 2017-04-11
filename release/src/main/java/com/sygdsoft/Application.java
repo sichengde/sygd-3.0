a9c0e2504130a59268ab965d96063b1d6fbc509a
shuzhan <2651283633@qq.com>
2017-04-11 12:22:17 +0800

    增加8080端口启动保护

--- release/src/main/java/com/sygdsoft/Application.java
+++ release/src/main/java/com/sygdsoft/Application.java
@@ -9,6 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * @author liuzh
  * @since 2015-12-12 18:22
@@ -21,6 +26,14 @@ public class Application extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         if (args.length>0) {
             System.out.println(args[0]);
+        }
+        try {
+            InetAddress theAddress = InetAddress.getByName("127.0.0.1");
+            Socket socket = new Socket(theAddress,8080);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            return;
         }
         SpringApplication.run(Application.class, args);
     }
