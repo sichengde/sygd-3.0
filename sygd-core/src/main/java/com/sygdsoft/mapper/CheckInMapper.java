1878002a30f095b2845c6fba428e7141b965aa77
shuzhan <2651283633@qq.com>
2017-06-15 09:19:11 +0800

    紧急修复预付不更新

--- sygd-core/src/main/java/com/sygdsoft/mapper/CheckInMapper.java
+++ sygd-core/src/main/java/com/sygdsoft/mapper/CheckInMapper.java
@@ -9,6 +9,6 @@ import org.apache.ibatis.annotations.Update;
  * Created by 舒展 on 2016-03-21.
  */
 public interface CheckInMapper extends MyMapper<CheckIn>{
-    @Update("UPDATE check_in SET consume=(SELECT sum(consume) FROM debt WHERE debt.room_id=#{roomId}) WHERE room_id=#{roomId}")
+    @Update("UPDATE check_in SET consume=(SELECT sum(consume) FROM debt WHERE debt.room_id=#{roomId}),deposit=(SELECT sum(deposit) FROM debt WHERE debt.room_id=#{roomId}) WHERE room_id=#{roomId}")
     void updateGuestInMoney(@Param("roomId")String roomId);
 }
