5f95ca360082b1f352189094e43a7c2d8d15d69f
shuzhan <sichengde1@163.com>
2017-09-21 16:01:00 +0800

    餐饮小优化

--- sygd-core/src/main/java/com/sygdsoft/mapper/DeskPayMapper.java
+++ sygd-core/src/main/java/com/sygdsoft/mapper/DeskPayMapper.java
@@ -27,20 +27,6 @@ public interface DeskPayMapper extends MyMapper<DeskPay> {
     Double getDeskMoneyByCurrencyDatePointOfSale(@Param("pointOfSale") String pointOfSale, @Param("beginTime") Date beginTime, @Param("endTime") Date endTime, @Param("currency") String currency);
 
     /**
-     * 获得该日期该币种的消费额(餐饮,不带销售员，带pos点（一级营业部门）)
-     */
-    @Select("select * from desk_pay where point_of_sale = #{pointOfSale} and done_time > #{beginTime} and done_time< #{endTime} and currency=#{currency} and ifnull(disabled,false)=false")
-    @Results(value = {
-            @Result(property = "doneTime", column = "done_time"),
-            @Result(property = "payMoney", column = "pay_money"),
-            @Result(property = "currencyAdd", column = "currency_add"),
-            @Result(property = "ckSerial", column = "ck_serial"),
-            @Result(property = "userId", column = "user_id"),
-            @Result(property = "pointOfSale", column = "point_of_sale")
-    })
-    List<DeskPay> getByCurrencyDatePointOfSale(@Param("pointOfSale") String pointOfSale, @Param("beginTime") Date beginTime, @Param("endTime") Date endTime, @Param("currency") String currency);
-
-    /**
      * 通过时间段和销售点统计币种
      */
     @Select("select currency,sum(pay_money) as payMoney from desk_pay where point_of_sale = #{pointOfSale} and done_time > #{beginTime} and done_time< #{endTime} and ifnull(disabled,false)=false group by currency")
