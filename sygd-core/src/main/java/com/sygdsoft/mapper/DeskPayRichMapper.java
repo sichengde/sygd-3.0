81e8ed1d1902c98767eea14bb88f0c40c21320ac
shuzhan <sichengde1@163.com>
2017-10-25 15:11:59 +0800

    修正虚拟营业点币种统计

--- sygd-core/src/main/java/com/sygdsoft/mapper/DeskPayRichMapper.java
+++ sygd-core/src/main/java/com/sygdsoft/mapper/DeskPayRichMapper.java
@@ -2,9 +2,17 @@ package com.sygdsoft.mapper;
 
 import com.sygdsoft.model.DeskPayRich;
 import com.sygdsoft.util.MyMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Date;
 
 /**
  * Created by 舒展 on 2017-03-16.
  */
 public interface DeskPayRichMapper extends MyMapper<DeskPayRich> {
+    @Select("SELECT ifnull(sum(dpr.pay_money),0) FROM desk_pay_rich dpr LEFT JOIN desk d ON dpr.desk=d.name WHERE dpr.currency=#{currency} and dpr.point_of_sale=#{pointOfSale} and d.category=#{category} and dpr.do_time>#{beginTime} and dpr.do_time<#{endTime}")
+    @ResultType(Double.class)
+    Double getPay(@Param("currency") String currency, @Param("pointOfSale") String pointOfSale, @Param("category") String category, @Param("beginTime") Date beginTime, @Param("endTime") Date endTime);
 }
