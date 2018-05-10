09ddee16f8776aaf70d01354001c422474631ffd
sichengde <sichengde1@163.com>
2018-05-10 22:40:00 +0800

    增加套餐始终在前功能

--- sygd-core/src/main/java/com/sygdsoft/sqlProvider/DeskDetailSql.java
+++ sygd-core/src/main/java/com/sygdsoft/sqlProvider/DeskDetailSql.java
@@ -11,10 +11,14 @@ public class DeskDetailSql {
     public String getList(Map<String, Object> parameters){
         String orderByList= (String) parameters.get("orderByList");
         String desk= (String) parameters.get("desk");
+        Boolean foodSet= (Boolean) parameters.get("foodSet");
         String basic;
         basic="select * from desk_detail where point_of_sale=#{pointOfSale}";
         if(desk!=null){
             basic+=" and desk=#{desk}";
+        }
+        if(foodSet!=null){
+            basic+=" and food_set=#{foodSet}";
         }
         if(orderByList!=null){
             basic+=" order by "+orderByList;
