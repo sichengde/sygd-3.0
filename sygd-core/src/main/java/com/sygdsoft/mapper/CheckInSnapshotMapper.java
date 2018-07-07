6200ef4e5ec9f8b1d431dc3c7e89b3705a537b7a
sichengde <sichengde1@163.com>
2018-07-07 08:19:44 +0800

    加参数

--- sygd-core/src/main/java/com/sygdsoft/mapper/CheckInSnapshotMapper.java
+++ sygd-core/src/main/java/com/sygdsoft/mapper/CheckInSnapshotMapper.java
@@ -1,6 +1,7 @@
 package com.sygdsoft.mapper;
 
 import com.sygdsoft.model.CheckInSnapshot;
+import com.sygdsoft.sqlProvider.CheckInSnapshotSql;
 import com.sygdsoft.util.MyMapper;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
@@ -12,6 +13,6 @@ public interface CheckInSnapshotMapper extends MyMapper<CheckInSnapshot>{
     @Delete("delete from room_state_report where report_time=#{debtDate}")
     void deleteByDate(@Param("debtDate") Date debtDate);
 
-    @SelectProvider(type = CheckInSnapshot.class,method = "getSum")
+    @SelectProvider(type = CheckInSnapshotSql.class,method = "getSum")
     Double getSum(@Param("field") String field, @Param("date") Date date);
 }
