6e2a1fe33b1695c984bab4d5fb9e63f42755c2c7
shuzhan <aiylove1@163.com.com>
2018-10-15 15:25:23 +0800

    细化权限，结账后押金单补打

--- room/src/main/java/com/sygdsoft/controller/DebtHistoryController.java
+++ room/src/main/java/com/sygdsoft/controller/DebtHistoryController.java
@@ -3,15 +3,19 @@ package com.sygdsoft.controller;
 import com.sygdsoft.jsonModel.OnlyString;
 import com.sygdsoft.jsonModel.Query;
 import com.sygdsoft.jsonModel.QuerySubReport;
+import com.sygdsoft.model.CheckInHistory;
+import com.sygdsoft.model.CheckInHistoryLog;
 import com.sygdsoft.model.DebtHistory;
 import com.sygdsoft.model.ReportJson;
-import com.sygdsoft.service.DebtHistoryService;
-import com.sygdsoft.service.GuestIntegrationService;
+import com.sygdsoft.service.*;
+import com.sygdsoft.util.SzMath;
+import com.sygdsoft.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -24,6 +28,20 @@ public class DebtHistoryController {
     DebtHistoryService debtHistoryService;
     @Autowired
     GuestIntegrationService guestIntegrationService;
+    @Autowired
+    ReportService reportService;
+    @Autowired
+    CheckInHistoryLogService checkInHistoryLogService;
+    @Autowired
+    CheckInHistoryService checkInHistoryService;
+    @Autowired
+    TimeService timeService;
+    @Autowired
+    OtherParamService otherParamService;
+    @Autowired
+    Util util;
+    @Autowired
+    SzMath szMath;
 
     @RequestMapping(value = "debtHistoryGet")
     public List<DebtHistory> debtHistoryGet(@RequestBody Query query) throws Exception {
@@ -61,5 +79,21 @@ public class DebtHistoryController {
     @RequestMapping("getTotalDepositByPaySerial")
     public Double getTotalDepositByPaySerial(@RequestBody String paySerial){
         return debtHistoryService.getTotalDeposit(paySerial);
+    }
+
+    /**
+     * 离店后补打押金单
+     */
+    @RequestMapping("depositDonePrintAgain")
+    public Integer depositDonePrintAgain(@RequestBody DebtHistory debtHistory)throws Exception{
+        try {
+            CheckInHistoryLog checkInHistoryLog=checkInHistoryLogService.getOneBySelfAccount(debtHistory.getSelfAccount());
+            List<CheckInHistory> checkInHistoryList = checkInHistoryService.getListBySelfAccount(checkInHistoryLog.getSelfAccount());
+            CheckInHistory checkInHistory=checkInHistoryList.get(0);
+            String guestNameString=checkInHistoryService.listToStringName(checkInHistoryList);
+            return reportService.generateReport(null, new String[]{guestNameString, checkInHistory.getSex(), timeService.dateToStringShort(checkInHistory.getBirthdayTime()), checkInHistory.getCardType(), checkInHistory.getCardId(), timeService.dateToStringLong(checkInHistoryLog.getReachTime()), timeService.dateToStringLong(checkInHistoryLog.getLeaveTime()), checkInHistoryLog.getRoomId(), String.valueOf(checkInHistoryLog.getFinalRoomPrice()), checkInHistoryLog.getVipNumber(), checkInHistory.getAddress(), debtHistory.getCurrency(), checkInHistoryLog.getImportant(), checkInHistoryLog.getRemark(), timeService.getNowLong(), debtHistory.getUserId(), debtHistory.getSelfAccount(), String.valueOf(debtHistory.getDeposit()), checkInHistoryLog.getCompany(), checkInHistoryLog.getRoomPriceCategory(), checkInHistoryLog.getProtocol(), checkInHistoryLog.getBreakfast(), otherParamService.getValueByName("酒店名称"), util.number2CNMontrayUnit(BigDecimal.valueOf(debtHistory.getDeposit())), "补打","",checkInHistoryLog.getGuestSource(),szMath.formatTwoDecimal(szMath.nullToZero(debtHistory.getDeposit())-szMath.nullToZero(checkInHistoryLog.getFinalRoomPrice())),checkInHistoryLog.getRoomCategory()}, "deposit", "pdf");
+        } catch (Exception e) {
+            throw new Exception("无法获取客人信息");
+        }
     }
 }
